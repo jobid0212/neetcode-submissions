7118c6f7278f9b41009e4f818f27a8b0be46c417
Jared Obidowski <jobid0212@gmail.com>
2026-05-14 19:35:43 -0500

    Add: validate-parentheses - submission-9

---
+++ Data Structures & Algorithms/validate-parentheses/submission-9.java
@@ -0,0 +1,40 @@
+class Solution {
+    public boolean isValid(String s) {
+        if (s.length() % 2 != 0) {
+            return false;
+        }
+        Deque<Character> stack = new ArrayDeque<>();
+
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            } else {
+                Character open = stack.poll();
+                if (open == null) {
+                    return false;
+                }
+                switch (open) {
+                    case '(':
+                        if (c != ')') {
+                            return false;
+                        }
+                        break;
+                    case '[':
+                        if (c != ']') {
+                            return false;
+                        }
+                        break;
+                    case '{':
+                        if (c != '}') {
+                            return false;
+                        }
+                        break;
+                    default:
+                        return false;
+                }
+            }
+        }
+
+        return (stack.size() == 0);
+    }
+}
