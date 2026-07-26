7fff6ca0cb072f3a05e3fa313f3d637f0db4085c
Jared Obidowski <jobid0212@gmail.com>
2026-07-26 15:07:55 -0500

    Add: car-fleet - submission-5

---
+++ Data Structures & Algorithms/car-fleet/submission-5.java
@@ -0,0 +1,33 @@
+class Solution {
+
+    // tuple
+    record Car(int position, int speed){}
+
+    public int carFleet(int target, int[] position, int[] speed) {
+        List<Car> cars = new ArrayList<>();
+
+        for (int i = 0; i < position.length; i++) {
+            cars.add(new Car(position[i], speed[i]));
+        }
+
+        cars.sort(Comparator.comparingInt(Car::position));
+        Deque<Car> carDeque = new ArrayDeque<>(cars);
+
+        int fleets = 1;
+
+        Car currentCar = carDeque.pollLast();
+        double prevTime = ((double)(target - currentCar.position())) / currentCar.speed();
+        double currentTime;
+        while (carDeque.size() > 0) {
+            currentCar = carDeque.pollLast();
+            currentTime = ((double)(target - currentCar.position())) / currentCar.speed();
+            if (prevTime < currentTime) {
+                fleets += 1;
+                prevTime = currentTime;
+            }
+
+        }
+
+        return fleets;
+    }
+}
